58eaecc1753b230554f4f1b65f2d4fb0ad11d200
zavbak <799000@mail.ru>
2017-04-20 21:40:11 +1000

    First

--- app/src/main/java/com/anit/alex/testapi/api/autorithation/AutoritationManager.java
+++ app/src/main/java/com/anit/alex/testapi/api/autorithation/AutoritationManager.java
@@ -9,13 +9,14 @@ public class AutoritationManager {
     public static String getStringAutorization(String username,String pass){
 
         if (username != null && pass != null) {
-            StringBuffer buf = new StringBuffer("Админ");
-            buf.append(':').append("админ1");
+            StringBuffer buf = new StringBuffer(username);
+            buf.append(':').append(pass);
             byte[] raw = buf.toString().getBytes();
             buf.setLength(0);
             buf.append("Basic ");
             org.kobjects.base64.Base64.encode(raw, 0, raw.length, buf);
 
+            //android.util.Base64.encode( auth.getBytes(), Base64.NO_WRAP);
             return buf.toString();
         }
 
